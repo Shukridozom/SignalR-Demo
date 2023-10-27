689f5e91580f4833934e7fa4e5ca6dffeaeb117a
Shukridozom <Shukridozom@hotmail.com>
2023-10-27 20:50:27 +0200

    adding a test method to chatHub

--- Project/ChatHub.cs
+++ Project/ChatHub.cs
@@ -4,5 +4,9 @@ namespace Project
 {
     public class ChatHub: Hub
     {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
     }
 }
